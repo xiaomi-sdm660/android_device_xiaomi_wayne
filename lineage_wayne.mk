b3fa8b75e526ed03ca805d5433e0c44dd457d162
Max Weffers <rcstar6696@gmail.com>
2020-01-02 14:25:15 +0100

    wayne: Treewide: Cleanup and Reorder
    * Remove Double Definition
    * Reordeder Alphabetic
    * Move Audio and Media Files to Configs
    * Move sensor files to configs
    * Reorder Properties
    * Fix whitespaces

--- lineage_wayne.mk
+++ lineage_wayne.mk
@@ -32,7 +32,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
+    PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
 
 # Device identifier
 PRODUCT_BRAND := Xiaomi
