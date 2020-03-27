85ecb0702e739dd63947fcbf4da598c1d8e893b4
Marcos_Almeida <36458823+Markdarkness@users.noreply.github.com>
2020-03-27 12:41:46 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # inherit MI 6X lineage device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_wayne.mk
+    $(LOCAL_DIR)/superior_wayne.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_wayne-eng \
-    bliss_wayne-userdebug \
-		bliss_wayne-user
+    superior_wayne-eng \
+    superior_wayne-userdebug \
+		superior_wayne-user
