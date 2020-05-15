f5852c30cfacd7bc7448103554f54d77eb76088f
GhostWayne <63330558+GhostWayne@users.noreply.github.com>
2020-05-15 03:47:36 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # inherit MI 6X lineage device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_wayne.mk
+    $(LOCAL_DIR)/potato_wayne.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_wayne-eng \
-    carbon_wayne-userdebug \
-		carbon_wayne-user
+    potato_wayne-eng \
+    potato_wayne-userdebug \
+		potato_wayne-user
