2cb4e203d223ad9968d9bae623f04e23887769ad
Max Weffers <rcstar6696@gmail.com>
2020-05-29 15:38:42 +0200

    wayne: Switch to new common vendor repo
    Change-Id: I9a85ebdc05468b50535803eccb8a3c5ed19c0697

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/media/MIUI_DualCamera_watermark_6X.png:$(TARGET_COPY_OUT_VENDOR)/etc/MIUI_DualCamera_watermark.png
 
 # Vendor files
-$(call inherit-product, vendor/xiaomi/wayne/wayne-vendor.mk)
+$(call inherit-product, vendor/xiaomi/wayne-common/wayne-common-vendor.mk)
 
 # AOSP DEVICE
 PRODUCT_NAME := aosp_wayne
