df88eaf4140fec3037be2a2ba7f53ed014a8239b
Max Weffers <rcstar6696@gmail.com>
2020-06-17 17:11:18 +0200

    wayne: Test old brightness setup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,13 +88,12 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # TWRP specific build flags
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_MAX_BRIGHTNESS := 4095
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_USE_TOOLBOX := true
