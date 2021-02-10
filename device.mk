aaa4e613b2d918d3c61d9cb4e4d53dfcd1efb9e7
OdSazib <odsazib@gmail.com>
2021-02-10 11:01:35 +0600

    Revert "wayne: powerhint: Set POWERHINT_VARIANT for SDM660"
    This reverts commit 769febc4ea96e848d298b173166422c7a8678f27.

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@
 # APEX
 ENABLE_APEX := true
 
-# POWERHINT VARIANT
-EAS_POWERHINT_VARIANT := sdm660
-
 # Inherit the fusion-common definitions
 $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
