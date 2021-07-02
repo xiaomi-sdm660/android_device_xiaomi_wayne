2c226cf3d6e24b71336030713946621c45f5f79d
Anush02198 <Anush.4376@gmail.com>
2021-07-02 21:45:58 +0530

    PBRP-ify
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- omni_wayne.mk
+++ omni_wayne.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := wayne
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=sdm660 \
